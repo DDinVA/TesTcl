a7052fb880d1c480e6808c259dd134339a9541ac
DDinVA <7632431+DDinVA@users.noreply.github.com>
2026-08-31 20:57:51 -0400

    test: add PSM 17.5 behavior coverage

--- test/test_emulator_adapter.py
+++ test/test_emulator_adapter.py
@@ -4781,6 +4781,7 @@ when HTTP_RESPONSE_RELEASE {
             "classification-17.5.json": 10,
             "ip-controls-17.5.json": 2,
             "profile-17.5.json": 25,
+            "psm-controls-17.5.json": 1,
             "psc-controls-17.5.json": 1,
             "pcp-controls-17.5.json": 1,
             "mr-17.5.json": 23,
